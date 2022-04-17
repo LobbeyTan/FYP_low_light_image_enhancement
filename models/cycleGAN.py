dd35010e9b30f66354f6a5ac7f0fe2fec37f940a
LobbeyTan <tancheelam2@gmail.com>
2022-04-17 21:47:26 +0800

    Update cycleGAN.py
    Change default blocks to 9
    Added identity loss
    Added test and eval function

--- models/cycleGAN.py
+++ models/cycleGAN.py
@@ -1,7 +1,10 @@
 
+from importlib.resources import path
 import itertools
 import os
+from unicodedata import name
 import torch
+import torchvision.transforms as transforms
 from networks.discriminators import NLayerDiscriminator
 from networks.generators import ResnetGenerator
 from networks.loss_functions import GANLoss
@@ -10,7 +13,7 @@ from torch import Tensor, nn
 
 class CycleGANModel:
 
-    def __init__(self, lr=0.001, lamda_A=10.0, lamda_B=10.0, lambda_idt=0.5, device=torch.device("cpu")) -> None:
+    def __init__(self, lr=0.001, lamda_A=10.0, lamda_B=10.0, lambda_idt=0.5, n_blocks=9, device=torch.device("cpu")) -> None:
         print("Creating cycleGAN on: %s" % device)
         self.device = device
 
@@ -18,10 +21,10 @@ class CycleGANModel:
         self.lambda_B = torch.tensor(lamda_B)
         self.lambda_idt = torch.tensor(lambda_idt)
 
-        self.G_X = ResnetGenerator(3, 3).to(self.device)
+        self.G_X = ResnetGenerator(3, 3, n_blocks=n_blocks).to(self.device)
         self.D_Y = NLayerDiscriminator(3).to(self.device)
 
-        self.F_Y = ResnetGenerator(3, 3).to(self.device)
+        self.F_Y = ResnetGenerator(3, 3, n_blocks=n_blocks).to(self.device)
         self.D_X = NLayerDiscriminator(3).to(self.device)
 
         self.criterionGAN = GANLoss().to(self.device)
@@ -66,10 +69,15 @@ class CycleGANModel:
 
     def backward_G(self):
 
-        self.loss_idt_A = self.criterionIdt(
-            self.G_X(self.real_Y), self.real_Y) * self.lambda_B * self.lambda_idt
-        self.loss_idt_B = self.criterionIdt(
-            self.F_Y(self.real_X), self.real_X) * self.lambda_A * self.lambda_idt
+        self.idt_X = self.G_X(self.real_Y)
+
+        self.loss_idt_X = self.criterionIdt(
+            self.idt_X, self.real_Y) * self.lambda_B * self.lambda_idt
+
+        self.idt_Y = self.F_Y(self.real_X)
+
+        self.loss_idt_Y = self.criterionIdt(
+            self.idt_Y, self.real_X) * self.lambda_A * self.lambda_idt
 
         self.loss_G_X = self.criterionGAN(self.D_Y(self.fake_Y), is_real=True)
         self.loss_F_Y = self.criterionGAN(self.D_X(self.fake_X), is_real=True)
@@ -81,7 +89,7 @@ class CycleGANModel:
             self.reconstruc_Y, self.real_Y) * self.lambda_B
 
         self.loss_G = self.loss_G_X + self.loss_F_Y + \
-            self.loss_cycle_X + self.loss_cycle_Y + self.loss_idt_A + self.loss_idt_A
+            self.loss_cycle_X + self.loss_cycle_Y + self.loss_idt_X + self.loss_idt_Y
 
         self.loss_G.backward()
 
@@ -138,3 +146,40 @@ class CycleGANModel:
         network.load_state_dict(torch.load(path, map_location=self.device))
 
         network.to(self.device)
+
+    def eval(self):
+        self.G_X.eval()
+        self.F_Y.eval()
+        self.D_X.eval()
+        self.D_Y.eval()
+
+    def test(self, real_X: Tensor, real_Y: Tensor, save_dir: str):
+        with torch.no_grad():
+            self.forward(real_X, real_Y)
+            return self.save_result(save_dir)
+
+    def save_result(self, save_dir):
+        paths = []
+        inverse_transform = transforms.Compose([transforms.Normalize([0., 0., 0.], [1/0.229, 1/0.224, 1/0.225]),
+                                                transforms.Normalize(
+                                                    [-0.485, -0.456, -0.406], [1., 1., 1.]),
+                                                transforms.ToPILImage()
+                                                ])
+
+        if not os.path.isdir(save_dir):
+            os.mkdir(save_dir)
+
+        def save(result: Tensor, name: str):
+            img = inverse_transform(result[0])
+            path = os.path.join(save_dir, f"{name}.jpg")
+            img.save(path, "JPEG")
+            paths.append((path, name))
+
+        save(self.real_X, "Real X")
+        save(self.real_Y, "Real Y")
+        save(self.fake_X, "Fake X")
+        save(self.fake_Y, "Fake Y")
+        save(self.reconstruc_X, "Reconstruct X")
+        save(self.reconstruc_Y, "Reconstruct Y")
+
+        return paths
