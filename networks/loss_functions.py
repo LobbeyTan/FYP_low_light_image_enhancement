4a20b7c8759d63e917b7ea5e644b7aa333d4dd8f
LobbeyTan <tancheelam2@gmail.com>
2022-04-18 12:44:45 +0800

    Update loss_functions.py
    Added MSE Loss

--- networks/loss_functions.py
+++ networks/loss_functions.py
@@ -5,13 +5,20 @@ import torch
 
 class GANLoss(nn.Module):
 
-    def __init__(self, target_real_label=1.0, target_fake_label=0.0) -> None:
+    def __init__(self, gan_mode="lsgan", target_real_label=1.0, target_fake_label=0.0) -> None:
         super(GANLoss, self).__init__()
 
         self.register_buffer('real_label', torch.tensor(target_real_label))
         self.register_buffer('fake_label', torch.tensor(target_fake_label))
 
-        self.loss = nn.BCEWithLogitsLoss()
+        self.gan_mode = gan_mode
+        
+        if gan_mode == "lsgan":
+            self.loss = nn.MSELoss()
+        elif gan_mode == "vanilla":
+            self.loss = nn.BCEWithLogitsLoss()
+        else:
+            raise NotImplementedError('gan mode %s not implemented' % gan_mode)
 
     def __call__(self, prediction, is_real=True) -> torch.Tensor:
         target_tensor = self.real_label if is_real else self.fake_label
