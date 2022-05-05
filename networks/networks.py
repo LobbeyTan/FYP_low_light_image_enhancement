71cd8df1c1b24c439a834091a7b8d87424450d0b
cheelam <tancheelam2@gmail.com>
2022-05-05 15:42:32 +0800

    Update networks.py

--- networks/networks.py
+++ networks/networks.py
@@ -81,7 +81,7 @@ def initNetWeight(m: nn.Module, method: WeightInit, init_gain=0.02):
                 raise NotImplementedError("Initialization method undefined")
         elif name.find('BatchNorm2d') != -1:
             nn.init.normal_(m.weight.data, 1, init_gain)
-            nn.init.constant_(m.bias.data, 0.0)
+            m.bias.data.fill_(0)
 
 
 class ConvLayer(nn.Module):
