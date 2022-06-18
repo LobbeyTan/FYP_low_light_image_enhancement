fbd667578de69bb98b9ee98eccfdae08657f518d
cheelam <tancheelam2@gmail.com>
2022-06-18 22:41:38 +0800

    Create train_enlighten.sh

---
+++ train_enlighten.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#SBATCH --partition=gpu-titan
+#SBATCH --job-name=Enlightening
+#SBATCH --output=%x.out
+#SBATCH --error=%x.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --mem=85G
+#SBATCH --qos=normal
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=tancheelam2@gmail.com
+#SBATCH --gres=gpu:1
+#SBATCH --hint=nomultithread
+
+source /home/user/lobbeytan/FYP_low_light_image_enhancement/venv/bin/activate
+
+cd /home/user/lobbeytan/FYP_low_light_image_enhancement/prior_works/enlightenGAN
+
+python script.py
