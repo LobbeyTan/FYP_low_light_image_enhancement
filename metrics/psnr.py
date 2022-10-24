4af54d09b9b289f5a4731923733d3fe903adadf2
LobbeyTan <tancheelam2@gmail.com>
2022-10-24 22:59:05 +0800

    Added evaluation metric

---
+++ metrics/psnr.py
@@ -0,0 +1,10 @@
+import numpy as np
+import math
+
+def psnr(original, contrast):
+    mse = np.mean((original - contrast) ** 2)
+    if mse == 0:
+        return 100
+    PIXEL_MAX = 255.0
+    PSNR = 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
+    return PSNR
